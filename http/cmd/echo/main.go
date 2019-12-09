32c03aa8bf7c2fd41e179b24a8d125c807632637
omeid matten <public@omeid.me>
2019-12-09 18:09:15 +1100

    Add sleep

--- http/cmd/echo/main.go
+++ http/cmd/echo/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	echo "github.com/omeid/echo/http"
 )
@@ -13,17 +14,21 @@ var (
 	host    string
 	port    string
 	reverse bool
+	wait    time.Duration
 )
 
 func init() {
 	flag.StringVar(&host, "host", "", "the host to listen on")
 	flag.StringVar(&port, "port", "3000", "the port to listen on")
 	flag.BoolVar(&reverse, "reverse", false, "whatever echo replies should be reversed.")
+	flag.DurationVar(&wait, "wait", 0, "Wait before listening")
 }
 
 func main() {
 
 	flag.Parse()
+
+	time.Sleep(wait)
 
 	addr := fmt.Sprintf("%s:%s", host, port)
 	server := echo.NewServer(reverse)
